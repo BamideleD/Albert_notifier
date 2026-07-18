a6d842558f18d15f19b745d6ed356fdfd110d432
BamideleD <bamadboss@gmail.com>
2026-07-18 12:41:07 +0100

    Scraper change

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 const runNotifier = require("./notifier");
 
-(async () => {
-    try {
-        await runNotifier();
-    } catch (err) {
-        console.error(err);
+const CHECK_INTERVAL = 5 * 60 * 1000; // 5 minutes
+
+async function start() {
+
+    console.log("Albert Notifier started.");
+
+    while (true) {
+
+        try {
+            await runNotifier();
+        } catch (err) {
+            console.error(err);
+        }
+
+        console.log("Sleeping for 5 minutes...\n");
+
+        await new Promise(resolve =>
+            setTimeout(resolve, CHECK_INTERVAL)
+        );
     }
-})();{}
+}
+
+start();
